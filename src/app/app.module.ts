5f4f7be0b1c1e791d8adab965d4005459cc65f71
chinhung <chinhung101@gmail.com>
2017-10-09 22:35:34 +0800

    add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdAlertBasic } from './alert/alert-basic';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -10,11 +12,13 @@ import { HomePage } from '../pages/home/home';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NgbdAlertBasic
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgbModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
